4fad9638b47ae1f1389a14fcb2c5919672ab7f11
naivedya-13 <tripathi1303@gmail.com>
2024-05-23 12:21:10 +0530

    day 2

--- src/app.js
+++ src/app.js
@@ -3,29 +3,36 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT || 8000;
 
+// Public static path
+const static_path = path.join(__dirname, "../public");
 
-// public static path
-const static_path = path.join(__dirname,"../public")
+// Set view engine to hbs
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "../views"));
 
-
+// Middleware to serve static files
 app.use(express.static(static_path));
 
-app.get("",(req,res)=>{
-    res.send("Welcome to first page")
+// Routes
+app.get("", (req, res) => {
+  res.render("index");
 });
 
-app.get("/about",(req,res)=>{
-    res.send("Welcome to about page")
+app.get("/about", (req, res) => {
+  res.render("about");
 });
 
-app.get("/weather",(req,res)=>{
-    res.send("Welcome to weather page")
+app.get("/weather", (req, res) => {
+  res.send("Welcome to weather page");
 });
 
-app.get("*",(req,res)=>{
-    res.send("404 Page Not Found")
+app.get("*", (req, res) => {
+  res.status(404).render("404", {
+    errorMsg: "Page Not Found"
+  });
 });
 
-app.listen(port,()=>{
-    console.log(`Listening at ${port}`)
-})
+// Start server
+app.listen(port, () => {
+  console.log(`Listening at ${port}`);
+});
